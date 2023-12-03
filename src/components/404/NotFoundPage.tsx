bad1a064d3927f722f4d217ce146f68734488990
Mohammad Amiri <aa511143@gmail.com>
2023-12-03 21:46:34 +0330

    bug fixing

--- src/components/404/NotFoundPage.tsx
+++ src/components/404/NotFoundPage.tsx
@@ -1,15 +1,14 @@
 import { Box, Typography } from "@mui/material"
-import Header from "../header/Header"
+import Link from "next/link"
 
 
 
 const NotFoundPage = () => {
   return (
-    <Box sx={{height: '100vh' , overflow :'hidden'}}> 
-    <Header />
-      <Box> 
-        <Typography  sx={{textAlign :'center' , mt :'20%' , fontSize : {xs : '16px' , md : '26px'}}}>404 - صفحه مورد نظر یافت نشد</Typography>
-
+    <Box sx={{ height: '100vh', overflow: 'hidden' }}>
+      <Box>
+        <Typography sx={{ textAlign: 'center', mt: '20%', fontSize: { xs: '16px', md: '26px' } }}>404 - صفحه مورد نظر یافت نشد</Typography>
+        <Link href={'/'}>  <Typography textAlign={'center'}>بازگشت به صفحه اصلی</Typography></Link>
       </Box>
 
     </Box>
